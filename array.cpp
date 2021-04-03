ba7bce1110158743f217b664cbe2bf96bc63c04d
Max <rudko.max10@gmail.com>
2021-04-03 17:13:18 +0300

    Implement array with incremental size increase

--- array.cpp
+++ array.cpp
@@ -1,6 +1,54 @@
 #include "array.h"
 
-Array::Array()
-{
-
+void Array::checkBounds(size_t index) const {
+    if (index < 0 || index >= getLength()) {
+        throw new std::invalid_argument("Array index is out of bounds!");
+    }
 }
+
+int IncrementalArray::operator[](size_t index) const {
+    checkBounds(index);
+
+    return data[index];
+}
+
+
+int &IncrementalArray::operator[](size_t index) {
+    checkBounds(index);
+
+    return data[index];
+}
+
+void IncrementalArray::alloc(size_t expectedCapacity)
+{
+    data = (int *) realloc(data, expectedCapacity);
+    capacity = expectedCapacity;
+}
+
+size_t IncrementalArray::getLength() const
+{
+    return length;
+}
+
+void IncrementalArray::add(int newElement) {
+    if (length + 1 >= capacity) {
+        alloc(length + 1);
+    }
+
+    data[length - 1] = newElement;
+    ++length;
+}
+
+void IncrementalArray::addAll(const Array &other)
+{
+    const size_t newLength = length + other.getLength();
+    if (newLength >= capacity) {
+        alloc(newLength);
+    }
+
+    for (size_t i = length; i < newLength; ++i) {
+        data[i] = other[i - length];
+    }
+    length = newLength;
+}
+
--- array.h
+++ array.h
@@ -1,11 +1,46 @@
 #ifndef ARRAY_H
 #define ARRAY_H
 
+#include <cstddef>
+#include <stdexcept>
+
+using std::size_t;
 
 class Array
 {
 public:
-    Array();
+    virtual int operator[](size_t index) const;
+    virtual int& operator[](size_t index);
+
+    virtual void add(int newElement);
+    virtual void addAll(const Array &other);
+
+    virtual size_t getLength() const;
+
+protected:
+    void checkBounds(size_t index) const;
+};
+
+class IncrementalArray: public Array {
+public:
+    IncrementalArray();
+    IncrementalArray(int initialSize);
+
+    int operator[](size_t index) const;
+    int &operator[](size_t index);
+
+    void add(int newElement);
+    void addAll(const Array &other);
+
+    size_t getLength() const;
+private:
+    const int defaultInitialSize = 4;
+
+    int *data = nullptr;
+    size_t length = 0;
+    size_t capacity = 0;
+
+    void alloc(size_t expectedCapacity);
 };
 
 #endif // ARRAY_H
